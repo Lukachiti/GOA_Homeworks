03d0780c65fc4113a2bf409e4663c57fc840b586
Lukachiti <chitidze.luka@outlook.com>
2024-12-01 18:45:38 +0400

    Add files via upload

---
+++ Day 59/homework/homework.js
@@ -0,0 +1,22 @@
+function funn() {
+    if(document.getElementById("emal").value == ""){}
+    else {
+        
+        console.log("Email - " + document.getElementById("emal").value)
+    }
+    if(document.getElementById("passwod").value == ""){}
+    else {
+        
+        console.log("Password - " + document.getElementById("passwod").value)
+    }
+    if(document.getElementById("nae").value == ""){}
+    else {
+        
+        console.log("Name - " + document.getElementById("nae").value)
+    }
+    if(document.getElementById("surnae").value == ""){}
+    else {
+        
+        console.log("Surname - " + document.getElementById("surnae").value)
+    }
+}
